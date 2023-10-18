ae04118d9baf27a22741a5784ebbdf4b1decbd89
M-H-Maruf <maruf.nightwalker@gmail.com>
2023-10-18 21:58:55 +0600

    my cart rout basic ui added

--- src/components/myCart/MyCart.jsx
+++ src/components/myCart/MyCart.jsx
@@ -2,11 +2,26 @@ import { Helmet } from "react-helmet-async";
 
 const MyCart = () => {
   return (
-    <div>
+    <div className=" bg-black/80 bg-[url('https://i.ibb.co/qWNH5fN/Alexandre-debieve-FO7-JIlwj-Ot-U-unsplash.jpg')] bg-cover bg-center bg-blend-darken text-white flex flex-col justify-center items-center tracking-normal text-justify">
       <Helmet>
         <title>Nexus | My Cart</title>
       </Helmet>
-      This is my cart
+      <div className="py-32 p-6">
+        <div className="max-w-6xl mx-auto">
+          <h1
+            data-aos="fade-down"
+            className="text-6xl text-brand-primary font-black font-orbitron mb-20 text-center mx-auto"
+          >
+            MY CART
+          </h1>
+          <div
+            data-aos="zoom-in"
+            className="bg-black/50 max-w-5xl m-8 md:m-24 p-6 md:p-10"
+          >
+
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
